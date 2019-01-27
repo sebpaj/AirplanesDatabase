c42f4110b576e3fb9332b02999db2d9bf219f3cc
Pajak, Sebastian <sebastian.pajak@accenture.com>
2019-01-27 18:18:28 +0100

    Adding update method

--- backend.py
+++ backend.py
@@ -81,9 +81,26 @@ def delete_from_table(airplane_id):
     conn.close()
 
 
-# view_table()
+def update_table(airplane_id, column, new_value):
+    """
+    Update value in row
 
+    :param airplane_id: integer
+    :param column: string
+    :param new_value: string/float/integer
+    :return: updated row
+    """
+    conn = psycopg2.connect("dbname='Airplanes' user='postgres' password='passtosql12' host='localhost' port='5432'")
+    cur = conn.cursor()
+    update_sql = "UPDATE airplanes SET {}=%s WHERE id=%s".format(column) % (new_value, airplane_id)
+    cur.execute(update_sql)
+    conn.commit()
+    conn.close()
+
+# view_table()
 # insert_into_table("707", "Boeing", "Passenger", 44.42, 46.61, 12.93, 0, 66406, 0, 972, 0)
 # insert_into_table("B-47E Statojet", "Boeing", "Military", 35.63, 33.48, 8.5, 132.7, 366300, 975, 0, 0.9)
 # delete_from_table(3)
+# view_table()
+# update_table(5, "mass", 36630)
 view_table()
